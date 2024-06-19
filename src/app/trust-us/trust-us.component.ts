cbe0c1e224d01c72f9302e584d0ee0e54f47ed7a
apizon <aleksanderpizon@gmail.com>
2024-06-19 22:58:39 +0200

    rebuilt trust-us

--- src/app/trust-us/trust-us.component.ts
+++ src/app/trust-us/trust-us.component.ts
@@ -1,13 +1,35 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-trust-us',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './trust-us.component.html',
-  styleUrl: './trust-us.component.scss',
+  styleUrls: ['./trust-us.component.scss']
 })
+
 export class TrustUsComponent {
+  @ViewChild('descriptionContainer', { static: true }) descriptionContainer!: ElementRef;
+
   hoveredIndex: number = -1;
+  lastHoveredIndex: number = -1;
+
+  setHoveredIndex(index: number): void {
+    if (index >= 0) {
+      this.lastHoveredIndex = index;
+    }
+    this.hoveredIndex = index;
+  }
+
+  getDescription(): string {
+    const items = document.querySelectorAll('.trust-us-item .description');
+    if (this.hoveredIndex >= 0) {
+      return items[this.hoveredIndex]?.textContent || '';
+    } else if (this.lastHoveredIndex >= 0) {
+      return items[this.lastHoveredIndex]?.textContent || '';
+    } else {
+      return 'Najedź na logo, aby zobaczyć opis';
+    }
+  }
 }
