d081ba0d53353497854a2caa2dcce12a8c17046d
apizon813 <148215546+apizon813@users.noreply.github.com>
2024-06-21 22:59:50 +0200

    fixed uppercase, nested styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
-  { path: 'about-us', component: AboutUsComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '/home' }
 ];
