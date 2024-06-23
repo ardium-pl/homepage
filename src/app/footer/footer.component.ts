90b86c647da375094fefc1676a84fb53ba7227d0
apizon <aleksanderpizon@gmail.com>
2024-06-23 13:43:22 +0200

    changed to contact service in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ContactService } from '../contact.service';
+
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  address: string = '';
+  phone: string = '';
+  email: string = '';
+
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    this.address = this.contactService.getAddress();
+    this.phone = this.contactService.getPhone();
+    this.email = this.contactService.getEmail();
+  }
+}
