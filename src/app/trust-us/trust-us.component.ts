3df6bd0d832ad98bcd801a8ad0dddfc199e82ea0
apizon <aleksanderpizon@gmail.com>
2024-06-25 13:15:49 +0200

    upgraded trust us still does not word

--- src/app/trust-us/trust-us.component.ts
+++ src/app/trust-us/trust-us.component.ts
@@ -11,29 +11,29 @@ import { CommonModule } from '@angular/common';
 export class TrustUsComponent {
   @ViewChild('descriptionContainer', { static: true }) descriptionContainer!: ElementRef;
 
-  hoveredIndex = signal<number>(0);  // Domyślnie ustawione na pierwsze logo
-  lastHoveredIndex = signal<number>(0);  // Domyślnie ustawione na pierwsze logo
+  hoveredIndex = signal<number>(0);  // Domyślnie ustawione na 0, co oznacza brak najechania
+  lastHoveredIndex = signal<number>(1);  // Domyślnie ustawione na pierwsze logo (1)
 
   description = computed(() => {
     const items = document.querySelectorAll('.trust-us-item .description');
-    if (this.hoveredIndex() >= 0) {
-      return items[this.hoveredIndex()]?.textContent || '';
-    } else if (this.lastHoveredIndex() >= 0) {
-      return items[this.lastHoveredIndex()]?.textContent || '';
+    if (this.hoveredIndex() > 0) {
+      return items[this.hoveredIndex() - 1]?.textContent || '';
+    } else if (this.lastHoveredIndex() > 0) {
+      return items[this.lastHoveredIndex() - 1]?.textContent || '';
     } else {
       return 'Najedź na logo, aby zobaczyć opis';
     }
   });
 
   setHoveredIndex(index: number): void {
-    if (index >= 0) {
+    if (index > 0) {
       this.lastHoveredIndex.set(index);
     }
     this.hoveredIndex.set(index);
   }
 
   getIconClass(index: number): string {
-    if (this.hoveredIndex() === index || (this.hoveredIndex() === -1 && this.lastHoveredIndex() === index)) {
+    if (this.hoveredIndex() === index || (this.hoveredIndex() === 0 && this.lastHoveredIndex() === index) || (this.hoveredIndex() === 0 && this.lastHoveredIndex() === 1 && index === 1)) {
       return 'big-icon';
     }
     return '';
