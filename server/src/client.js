7cdf3f38f79785471377825128fc47c7b6eb4c29
Jakub <brodka.jakub@gmail.com>
2024-06-26 15:50:14 +0200

    console.log remove

--- server/src/client.js
+++ server/src/client.js
@@ -3,12 +3,10 @@ import express from "express";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-console.log(__dirname);
 
 const router = express.Router();
 
 const angularAppPath = path.join(__dirname, "../../dist/homepage/browser");
-console.log(angularAppPath);
 
 // Serve static files for the root URL
 router.use('/', express.static(angularAppPath));
