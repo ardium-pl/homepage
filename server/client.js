29a6846868fe7e90d8a3ca651c79632c6235e47e
lolocob7 <147922448+lolocob7@users.noreply.github.com>
2024-07-04 20:18:06 +0200

    backend fix (#7)
    Co-authored-by: Jakub <brodka.jakub@gmail.com>

---
+++ server/client.js
@@ -0,0 +1,18 @@
+import path from "path";
+import express from "express";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const router = express.Router();
+
+const angularAppPath = path.join(__dirname, "../dist/homepage/browser");
+
+// Serve static files for the root URL
+router.use('/', express.static(angularAppPath));
+// Serve static files for any other URL
+router.use('*', express.static(angularAppPath));
+
+
+
+export default router;
