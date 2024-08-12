ccef6a505f3f342fc883d0495532236b19d85b39
joker876 <joker876x@gmail.com>
2024-08-12 21:41:01 +0200

    improve some section styling + add address link

--- client2/src/app/components/office-card/office-card.component.ts
+++ client2/src/app/components/office-card/office-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, input } from '@angular/core';
 import { CardModule } from '@components/card';
 import { createMailto } from '@utils/mailto';
+import { createUniversalMapLink } from '@utils/maps-link';
 import { CallCallingIcon } from '../../icons/call-calling.icon';
 import { EnvelopeIcon } from '../../icons/envelope.icon';
 
@@ -17,6 +18,7 @@ export class OfficeCardComponent {
   readonly email = input.required<string>();
   readonly phone = input.required<string>();
 
+  readonly addressHref = computed(() => createUniversalMapLink(this.address()));
   readonly emailHref = computed(() => createMailto(this.email()));
   readonly phoneHref = computed(() => `tel:${this.phone().replace(/\s/g, '')}`);
 }
---
+++ client2/src/app/utils/maps-link.ts
@@ -0,0 +1,5 @@
+
+export function createUniversalMapLink(address: string): string {
+  const encodedAddress = encodeURIComponent(address);
+  return `https://maps.google.com/?q=${encodedAddress}`;
+}
