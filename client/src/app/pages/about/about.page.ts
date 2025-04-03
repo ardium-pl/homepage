917db37f643a34408921bddb15add8d79e9a584c
joker876 <joker876x@gmail.com>
2025-04-03 21:35:59 +0200

    create extended about us page

--- client/src/app/pages/about/about.page.ts
+++ client/src/app/pages/about/about.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ExtendedAboutComponent } from "./sections/extended-about/extended-about.component";
 import { StaffSection } from './sections/staff/staff.section';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [StaffSection],
+  imports: [StaffSection, ExtendedAboutComponent],
   templateUrl: './about.page.html',
   styleUrl: './about.page.scss'
 })
