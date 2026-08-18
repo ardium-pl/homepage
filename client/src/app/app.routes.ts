dc50cc53d4c6bc6793628567aeaa29bf0a6d571b
AgnieszkaPrzezdziecka <agnieszka.przezdziecka@ardium.pl>
2026-08-18 18:20:41 +0200

    fix

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { AboutPage } from '@pages/about';
 import { BlogPage } from '@pages/blog';
 import { BlogPostPage } from '@pages/blog-post';
 import { HomePage } from '@pages/home';
-import { PrivacyPolicyPage } from '@pages/privacy-policy/privacy-policy.page';
+import { PrivacyPolicyPage } from '@pages/privacy-policy';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
--- client/src/app/icons/shield-check.icon.ts
+++ client/src/app/icons/shield-check.icon.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   template: `<svg
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    focusable="false"
     x="0px"
     y="0px"
     viewBox="0 0 24 24"
